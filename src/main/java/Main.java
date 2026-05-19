8e04c78b9abf1da6716c0f8751878bca264e9b35
Norhan <nourhanayman581@gmail.com>
2026-05-20 01:52:41 +0300

    single-quote

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -63,7 +63,13 @@ public class Main {
 
             }
             else if(command.startsWith("echo") || command.startsWith(" echo"))
-                System.out.println(command.substring(5).replaceAll("'",""));
+            {
+                String text = command.substring(5).trim().replaceAll("'","");
+                String [] words =text.split(" +");
+                System.out.println(String.join(" ", words));
+
+
+            }
             else if (command.startsWith("pwd"))
             {
                 String currentPath=System.getProperty("user.dir");
