09bd2c09197796c5232002d6c4c781be7c347702
Norhan <nourhanayman581@gmail.com>
2026-05-19 19:09:47 +0300

    exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
+
         while(true) {
 
 
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String command = scanner.nextLine();
+            if(command.equals("exit"))
+                return;
             System.out.println(command + ": command not found");
         }
     }
